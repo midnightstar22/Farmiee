e142314c943a59d6aff371864fa9748c6420c645
Nakshatra__RM <121953200+midnightstar22@users.noreply.github.com>
2025-03-11 11:07:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Home from './screens/Home';
-import Recommendation from './screens/Recommendation';
 import{
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
 import Login from'./screens/Login';
-
+import Store from './screens/Store';
+import Profile from './screens/Profile'
+import Signup  from './screens/Signup';
+import Recommendation from './screens/Recommendation';
 import "bootstrap/dist/css/bootstrap.min.css";  
 
 import "bootstrap/dist/js/bootstrap.bundle.min"; 
@@ -18,8 +20,14 @@ function App() {
     <div > 
       <Routes>
         <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/about" element={<Home/>}/>
+
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path='/Recommendation' element={<Recommendation />} />
+        <Route exact path="/nearby-store" element={<Store/>}/>
+        <Route exact path="/profile" element={<Profile/>}/>
+        <Route exact path="/signup" element={<Signup/>}/>
+
       </Routes>
     </div>
     </Router>
