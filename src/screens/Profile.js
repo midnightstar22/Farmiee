d167a1287cc01d336a2f35d32a24710c3b8e83fd
Nakshatra__RM <121953200+midnightstar22@users.noreply.github.com>
2025-03-11 11:10:49 +0530

    Create Profile.js

---
+++ src/screens/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from "../components/Navbar";
+
+import Prof from '../components/Prof'
+function Profile() {
+  return (
+    <div>
+      <div>
+            <Navbar />
+          </div>
+    
+    <div>
+      <Prof/>
+    </div>
+    </div>
+  )
+}
+
+export default Profile
