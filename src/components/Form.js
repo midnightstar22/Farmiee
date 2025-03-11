d3b0a6924547477135ffc1c48625e891f5a240f8
Nakshatra__RM <121953200+midnightstar22@users.noreply.github.com>
2025-03-11 11:07:57 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,47 +1,42 @@
-import React from 'react';
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Signup from "./Up";
 export default function Form() {
-  return (
-    <div>
-      <section className="vh-100">
-        <div className="container py-5 h-100">
-          <div className="row d-flex align-items-center justify-content-center h-100">
-            <div className="col-md-8 col-lg-7 col-xl-6">
-              <img src="/imgs/7341255.jpg" className="img-fluid w-100 h-100" style={{ objectFit: 'cover' }} alt="Phone image" />
-            </div>
-            <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-              <form>
-                <div className="form-outline mb-4">
-                  <input type="email" id="form1Example13" className="form-control form-control-lg" />
-                  <label className="form-label" htmlFor="form1Example13">Email address</label>
-                </div>
-                <div className="form-outline mb-4">
-                  <input type="password" id="form1Example23" className="form-control form-control-lg" />
-                  <label className="form-label" htmlFor="form1Example23">Password</label>
-                </div>
-                <div className="d-flex justify-content-around align-items-center mb-4">
-                  <div className="form-check">
-                    <input className="form-check-input" type="checkbox" value="" id="form1Example3" defaultChecked />
-                    <label className="form-check-label" htmlFor="form1Example3"> Remember me </label>
-                  </div>
-                  <a href="#!" style={{ backgroundColor: '#5B913B' }}>Forgot password?</a>
-                </div>
-                <button type="submit" className="btn btn-primary btn-lg btn-block"style={{ backgroundColor: '#5B913B' }}>Sign in</button>
-                <div className="divider d-flex align-items-center my-4">
-                  <p className="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
-                </div>
-                <a className="btn btn-primary btn-lg btn-block" style={{ backgroundColor: '#5B913B' }} href="#!" role="button">
-                  <i className="fab fa-facebook-f me-2"></i>Continue with Facebook
+  const navigate = useNavigate();
 
-                </a>
-                <a className="btn btn-primary btn-lg btn-block" style={{ backgroundColor: '#77B254' }} href="#!" role="button">
-                  <i className="fab fa-twitter me-2"></i>Continue with Twitter
-                </a>
-              </form>
-            </div>
-          </div>
+  return (
+    <div className="container vh-100 d-flex align-items-center justify-content-center">
+      <div className="row w-100">
+        <div className="col-md-6 d-none d-md-block">
+          <img src="/imgs/7341255.jpg" className="img-fluid rounded" alt="Phone image" />
         </div>
-      </section>
+        <div className="col-md-6">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Password</label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+
+            <div className="d-flex justify-content-between align-items-center mb-3">
+              <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="remember" defaultChecked />
+                <label className="form-check-label" htmlFor="remember">Remember me</label>
+              </div>
+              <a href="#" className="text-primary">Forgot password?</a>
+            </div>
+
+            <div className="d-grid gap-2">
+              <button type="submit" className="btn btn-success">Sign In</button>
+              <button type="button" className="btn btn-primary" onClick={() => navigate("/Signup")}>Sign Up</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
